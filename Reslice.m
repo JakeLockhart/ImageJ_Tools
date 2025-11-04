02e5f641776488f18dd2c304d71de7870879d167
JakeLockhart <jakelockhart65@gmail.com>
2025-11-03 21:50:39 -0500

    Fixed syntax error for switch case

--- Reslice.m
+++ Reslice.m
@@ -7,9 +7,9 @@ function ReslicedStack = Reslice(Stack, Orientation)
     switch Orientation
         case "XY"
             ReslicedStack = permute(Stack, [1,2,3]);
-        case "XZ"
-            ReslicedStack = permute(Stack, [2,1,3]);
         case "YZ"
+            ReslicedStack = permute(Stack, [2,1,3]);
+        case "XZ"
             ReslicedStack = permute(Stack, [3,2,1]);
     end
 end
