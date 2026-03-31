86b1093f3793ba2968333017d8eb1becd9dcb49d
Jake Lockhart <jsl5865@psu.edu>
2026-03-31 15:02:29 -0400

    Created a way to mimic ImageJ's import image sequence.

---
+++ File/Import/ImageSequence.m
@@ -0,0 +1,71 @@
+function imageStack = ImageSequence
+    % <Documentation>
+        % ImageSequence()
+        %   
+        %   Created by: jsl5865
+        %   
+        % Syntax:
+        %   
+        % Description:
+        %   
+        % Input:
+        %   
+        % Output:
+        %   
+    % <End Documentation>
+    
+    zap
+    fprintf('Select tif file(s) to load...\n')
+    [fileName, folderPath] = userSelectFiles;
+    validateImportSelection;
+    paths = fileExplorer;
+    imageStack = loadSequence;
+    fprintf('File(s) loaded ✓')
+    
+
+
+    function [fileName, folderPath] = userSelectFiles()
+        [fileName, folderPath] = uigetfile('*.tif', 'Import tif files...', ...
+                                                'MultiSelect', 'on');
+    end
+
+    function validateImportSelection()
+        if isequal(fileName, 0)
+            Identifier = 'ImportSelection::NoFileSelected';
+            ME = MException(Identifier, 'No files selected to import.');
+            throwAsCaller(ME)
+        end
+
+        if ischar(fileName)
+            fileName = {fileName};
+        end
+    end
+
+    function paths = fileExplorer()
+        paths = string(fileName);
+        for file = 1:numel(fileName)
+            paths(file) = fullfile(folderPath, fileName{file});
+        end
+    end
+
+    function imageStack = loadSequence()
+        imageStack = [];
+
+        for file = 1:numel(paths)
+            Timer = tic;
+            fileInfo = imfinfo(paths(file));
+            frames = numel(fileInfo);
+            rows = fileInfo(1).Height;
+            columns = fileInfo(1).Width;
+            subStack = zeros(rows, columns, frames, "like", imread(paths(file), 1));
+            
+            for frame = 1:frames
+                subStack(:,:,frame) = imread(paths(file), frame);
+            end
+            
+            importTime = toc(Timer);
+            fprintf('\tLoaded file %d (%.3gs)\n', file, importTime)
+            imageStack = cat(3, imageStack, subStack);
+        end
+    end
+end
